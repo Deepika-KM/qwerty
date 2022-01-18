b3426cd7f5c671453c62c651df77f89584af0cc0
dekr <dekr@host.docker.internal>
2022-01-18 17:32:58 +0530

    pr

--- spring-boot-docker-mavens/src/main/java/com/example/spring/docker/api/SpringBootDockerMavensApplication.java
+++ spring-boot-docker-mavens/src/main/java/com/example/spring/docker/api/SpringBootDockerMavensApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDockerMavensApplication {
 	
 	public static void main(String[] args) {
-		System.out.print("pop");
+		System.out.print("pop ");
 		SpringApplication.run(SpringBootDockerMavensApplication.class, args);
 	}
 
