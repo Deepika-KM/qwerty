ecf9983f689fda6743f600b959ad8243f7905709
dekr <dekr@host.docker.internal>
2022-01-18 11:21:59 +0530

    fc

---
+++ spring-boot-docker-mavens/src/main/java/com/example/spring/docker/api/SpringBootDockerMavensApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.docker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMavensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMavensApplication.class, args);
+	}
+
+}
---
+++ spring-boot-docker-mavens/src/test/java/com/example/spring/docker/api/SpringBootDockerMavensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.docker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
